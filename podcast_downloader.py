65bae9a41e5a3f4468c9f090e0fc3d39ac7b4b4c
Elias Maier <39457447+eeli1@users.noreply.github.com>
2023-01-21 22:35:21 +0100

    Update podcast_downloader.py

--- podcast_downloader.py
+++ podcast_downloader.py
@@ -1,41 +1,46 @@
 import feedparser
-import requests
 import os
 import sys
 
-def downland(url,file_path ):
-    if not os.exists(file_path):
-        os.system("wget {} -O {}".format(url,file_path))
-    
+
+def downland(url, path, filename):
+    if not os.path.isfile(os.path.join(path, filename)):
+        os.system("cd {} && wget {} -O {}".format(path, url, filename))
+
+
 def get_audio(url, path, title):
-    filename = title.replace(' ','_')+".mp3"
-    filename = filename.replace("/","")
-    file_path = os.path.join(path, filename)
-    downland(url,file_path)
-    
+    filename = title.replace(' ', '_')+".mp3"
+    filename = filename.replace("/", "")
+    downland(url, path, filename)
+
+
 def get_audio_url(links):
     for e in links:
         if "audio" in e["type"]:
             return e["href"]
     raise Exception("no audio found")
 
+
 def download_rss(url, base_path):
     feed = feedparser.parse(url)
     title = feed["feed"]["title"]
-    path = os.path.join(base_path,title.replace(' ','_'))
-    os.mkdir(path)
+    title = title.replace(' ', '_')
+    title = title.replace("'", '')
+    path = os.path.join(base_path, title)
+    if not os.path.exists(path):
+        os.mkdir(path)
 
-    downland(url,os.path.join(path,"rss"))
+    downland(url, path, "rss")
 
-    image_url =  feed["feed"]["image"]["href"]
-    image_name = title.replace(' ','_')+".jpg"
-    image_path = os.path.join(path,image_name)
-    downland(image_url,image_path)
+    image_url = feed["feed"]["image"]["href"]
+    image_name = title.replace(' ', '_')+".jpg"
+    downland(image_url, path, image_name)
 
     for e in feed["entries"]:
         audio_url = get_audio_url(e["links"])
         audio_title = e["title"]
         get_audio(audio_url, path, audio_title)
+
 
 def main(argv):
     if len(argv) != 3:
@@ -48,7 +53,8 @@ def main(argv):
     file1 = open(argv[1], 'r')
     lines = file1.readlines()
     for url in lines:
-        download_rss(url,base_path)
+        download_rss(url, base_path)
+
 
 if __name__ == "__main__":
-   main(sys.argv)
+    main(sys.argv)
